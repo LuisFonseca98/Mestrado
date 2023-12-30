a36d974f136b458e309caa83e149d05f1b559690
Luis Fonseca <97262672+LuisFonseca98@users.noreply.github.com>
2023-12-30 01:45:46 +0000

    ES

---
+++ ES/Es_Tunespot_Project/tunespot/src/App.js
@@ -0,0 +1,10 @@
+import "bootstrap/dist/css/bootstrap.min.css"
+import Login from "./Login"
+
+const code = new URLSearchParams(window.location.search).get("code")
+
+function App() {
+  return <Login />
+}
+
+export default App
